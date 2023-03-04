a5b0685d73358c86072d236290ce1358cc8d1303
zaharlol <kovtun.zahar12@mail.ru>
2023-03-04 14:41:21 +0300

    Работа с текстом

--- NameSpace/Program.cs
+++ NameSpace/Program.cs
@@ -1,20 +1,39 @@
-﻿
-using System.Net.Security;
+﻿using System.Net.Security;
 
 class Program
 {
     private static void Main(string[] args)
     {
         {
-            string Name = "Захар";
-            byte year = 21;
-            bool back = true ;
-            double ttt = 44.5;
+            {
+                string MyName;
+                MyName = "Захар";
+                const string Name1 = "Захар";
+                Console.WriteLine(MyName);
+                Console.WriteLine(Name1);
+                Console.ReadKey();
 
-            Console.WriteLine("Меня зовут " + Name);
-            Console.WriteLine("Мне " + year + " год");
-            Console.WriteLine("У вас есть питиомец? " + back);
-            Console.WriteLine("Размер ноги " + ttt);
+                Console.WriteLine("\tПривет, мир");
+                Console.WriteLine("Мне 27 лет");
+                Console.WriteLine("My name is Jane");
+                Console.ReadKey();
+
+                Console.WriteLine("Привет,\n мир!");
+                Console.WriteLine('\x40');
+                Console.WriteLine('\x23');
+                Console.ReadKey();
+
+
+                string Name = "Захар";
+                byte year = 21;
+                bool back = true;
+                double ttt = 44.5;
+
+                Console.WriteLine("Меня зовут " + Name);
+                Console.WriteLine("Мне " + year + " год");
+                Console.WriteLine("У вас есть питиомец? " + back);
+                Console.WriteLine("Размер ноги " + ttt);
+            }
         }
        
     }
