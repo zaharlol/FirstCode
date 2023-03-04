46710c53ac534783190de56a8c7fa9260a98a99d
zaharlol <kovtun.zahar12@mail.ru>
2023-03-04 14:31:38 +0300

    Проверка переменных

--- NameSpace/Program.cs
+++ NameSpace/Program.cs
@@ -1,9 +1,21 @@
 ﻿
+using System.Net.Security;
+
+class Program
 {
-    string MyName;
-    MyName = "Захар";
-    Console.WriteLine(MyName);
-    Console.ReadKey();
-    MyName = "Другое имя";
-    Console.WriteLine(MyName);
+    private static void Main(string[] args)
+    {
+        {
+            string Name = "Захар";
+            byte year = 21;
+            bool back = true ;
+            double ttt = 44.5;
+
+            Console.WriteLine("Меня зовут " + Name);
+            Console.WriteLine("Мне " + year + " год");
+            Console.WriteLine("У вас есть питиомец? " + back);
+            Console.WriteLine("Размер ноги " + ttt);
+        }
+       
+    }
 }
