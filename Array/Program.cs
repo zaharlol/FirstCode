ff0e4f3f09f9476605b5d694666160c64ad89ec2
zaharlol <kovtun.zahar12@mail.ru>
2023-03-14 12:19:20 +0300

    Имя

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Security;
+using System.Reflection;
+using System.Xml.Linq;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свё имя");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Ваше имя по буквам: ");
+
+        foreach (var q in name) 
+        {
+            Console.Write(q + " ");
+        }
+
+        Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+        Console.ReadKey();
+    }
+}
