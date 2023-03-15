e2ef54b5038e7ffe7e4b45a817044402dbc7bfcb
zaharlol <kovtun.zahar12@mail.ru>
2023-03-15 12:33:08 +0300

    Кортеж

---
+++ Cort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Security;
+using System.Reflection;
+using System.Xml.Linq;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        (string name, int age) anketa;
+
+        Console.WriteLine("Как вас зовут?");
+        anketa.name = Console.ReadLine();
+
+        Console.WriteLine("Сколько вам лет?");
+        anketa.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Вас зовут {0}", anketa.name);
+        Console.WriteLine("Ваш возраст {0}", anketa.age);
+    }
+}
